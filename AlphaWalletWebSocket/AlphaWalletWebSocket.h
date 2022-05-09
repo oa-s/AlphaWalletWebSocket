c64fe07a6b64a63c1f98201012761c51e1f3e162
Krypto Pank <krypto.pank@gmail.com>
2022-05-09 10:42:30 +0300

    Initial project setup

---
+++ AlphaWalletWebSocket/AlphaWalletWebSocket.h
@@ -0,0 +1,18 @@
+//
+//  AlphaWalletWebSocket.h
+//  AlphaWalletWebSocket
+//
+//  Created by Vladyslav Shepitko on 09.05.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AlphaWalletWebSocket.
+FOUNDATION_EXPORT double AlphaWalletWebSocketVersionNumber;
+
+//! Project version string for AlphaWalletWebSocket.
+FOUNDATION_EXPORT const unsigned char AlphaWalletWebSocketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlphaWalletWebSocket/PublicHeader.h>
+
+
